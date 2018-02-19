fe6920458361992e3e17974e684323d4134f081c
Lu Lin <lulin@Lus-MacBook-Pro.local>
2018-02-19 13:40:57 -0800

    add Stickerpage

---
+++ src/Sticker.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+
+class Sticker extends Component {
+    
+    
+    
+    
+  render() {
+      
+          
+      
+    
+    return (
+        <div>
+        
+        
+      <p>Welcome to sticker page</p>
+        </div>
+    );
+  }
+}
+
+export default Sticker;
